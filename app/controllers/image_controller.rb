207164cf0fff93232086ab2dd8e4da5141fe755c
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-04 10:17:14 -0400

    built the index and create actions in the image controller

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,6 +1,25 @@
 class ImageController < ApplicationController
 
-  
+  def index
+    render json: Image.all
+  end
+
+  def create
+    @image = Image.new(image_params)
+    if @image.save
+      render json: @user
+    else
+      render json: {errors: @image.errors}, status: 400
+    end
+  end
+
+
+
+  private
+
+  def image_params
+    params.require(:image).permit(:imageURL, :name, :description)
+  end
 
 
 
