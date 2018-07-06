21f27c419f9414179ab9cb2697f69670c12d7c28
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-05 22:03:21 -0400

    Added get images to the reducers

--- client/src/reducers/imagesreducer.js
+++ client/src/reducers/imagesreducer.js
@@ -2,6 +2,8 @@ export default (state = [], action) => {
   switch (action.type) {
     case 'ADD_IMAGE':
       return state.concat(action.image)
+    case 'GET_IMAGES':
+      return action.breeds
     default:
       return state;
   }
