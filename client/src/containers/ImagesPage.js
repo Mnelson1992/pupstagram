f5cf6cb9612a6a495050a77ccf2ea3fd148bfc00
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-05 21:43:30 -0400

    Built out the Images Page

---
+++ client/src/containers/ImagesPage.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+class ImagesPage extends Component {
+
+
+  render () {
+    return (
+      <div>
+        <Route exact path={this.props.match.url} render={() => <h4>Select an Image from the list to learn more.</h4>} />
+        <ImagesIndex images={this.props.images} />
+        <Switch>
+          <Route path={`${this.props.match.url}/new`} component={ImagesNew} />
+          <Route path={`${this.props.match.url}/:imageId`} component={ImagesShow}/>
+        </Switch>
+      </div>
+      );
+    }
+  }
+
+  const mapStateToProps = state => {
+    return {
+      images: state.images
+  };
+
+}
+
+
+
+export default connect(mapStateToProps)(ImagesPage);
