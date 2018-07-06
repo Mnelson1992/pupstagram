6cff3010e6018383d4eb75a8a75c2236ff2c66d3
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-06 09:18:49 -0400

    Laid out the baic page structure for Images New

---
+++ client/src/containers/ImagesNew.js
@@ -0,0 +1,15 @@
+import react, { Component } from 'react';
+
+class ImagesNew extends Component {
+
+
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
+};
+
+export default (ImagesNew)
