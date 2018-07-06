4c5ab4caa7cb5af70103e3bf4a739a8d52ba1c5a
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-05 21:27:05 -0400

    Added text to the home page

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () =>
   <div>
-    <h1>Welcome Home!</h1>
+    <h1>Welcome to Pupstagram! The best place to browse pictures of pups.</h1>
   </div>
 
 export default Home;
