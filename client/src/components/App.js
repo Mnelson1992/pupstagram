fa6c5af4f5798e3fec9c30b80ba00c278606c015
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-05 20:14:07 -0400

    Cleaned up the App.js file to add components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,12 +4,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-          <h1>Hello, I am matthew</h1>
-        </header>
-        <p className="App-intro">
-        </p>
+        
       </div>
     );
   }
