0d531284fe1378820ac453ba1292c537e2e5ce7b
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-05 21:50:01 -0400

    Added the route to ImagesPage

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Home from './Home'
 import Navbar from './Navbar'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import ImagesPage from '../containers/ImagesPage';
+
 
 
 const App = () =>
@@ -9,7 +11,7 @@ const App = () =>
       <div className="App">
         <Navbar />
         <Route exact path="/" component={Home} />
-
+        <Route path="/images" component={ImagesPage} />
       </div>
     </Router>
 
