b2f1820010dbc84cd1f10ffd58bc12ca006c5978
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-05 20:27:47 -0400

    Added the navbar for routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Home from './Home'
+import Navbar from './Navbar'
 
 const App = () =>
 
       <div className="App">
         <Home />
+        <Navbar />
       </div>
 
 
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Navbar = () =>
+  <div>
+    <h1>I am the navbar</h1>
+  </div>
+
+export default Navbar;
