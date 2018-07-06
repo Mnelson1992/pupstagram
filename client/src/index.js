92ca9e215767873a64c59cc4b2c6d6ea75e660d6
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-05 22:07:59 -0400

    Imported rootreducer

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import {Provider} from 'react-redux';
 import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 import imagesReducer from './reducers/imagesReducer'
+import rootReducer from './reducers/index';
+
 
 const store = createStore(imagesReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
