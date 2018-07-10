b32e7446cc72357ef632e9f4a3be403c63e708b8
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-09 21:37:06 -0400

    Changed seed data to represent a .jpg image extension

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,22 +6,22 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Image.create(
-  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjZkMWk84XcAhWus1kKHRd5CwgQjRx6BAgBEAU&url=http%3A%2F%2Fdogtime.com%2Fpuppies%2F255-puppies&psig=AOvVaw0gfpk0XwYsU41IHpp44Srz&ust=1530809330470565',
+  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjZkMWk84XcAhWus1kKHRd5CwgQjRx6BAgBEAU&url=http%3A%2F%2Fdogtime.com%2Fpuppies%2F255-puppies&psig=AOvVaw0gfpk0XwYsU41IHpp44Srz&ust=1530809330470565.jpg',
   name: 'Cooper',
   description: 'Cooper enjoying the warm weather today!')
 Image.create(
-  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiwkrKq9oXcAhWKxVkKHdyuAGIQjRx6BAgBEAU&url=https%3A%2F%2Fcheezburger.com%2F1108997%2Fits-a-shame-that-these-dogs-will-never-know-how-funny-they-are-on-snapchat&psig=AOvVaw0w483W3GZVJDiULDmG1lKj&ust=1530810138691688',
+  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiwkrKq9oXcAhWKxVkKHdyuAGIQjRx6BAgBEAU&url=https%3A%2F%2Fcheezburger.com%2F1108997%2Fits-a-shame-that-these-dogs-will-never-know-how-funny-they-are-on-snapchat&psig=AOvVaw0w483W3GZVJDiULDmG1lKj&ust=1530810138691688.jpg',
   name: 'Benji',
   description: 'I just finished mowing the lawn and turn around to see this.....')
 Image.create(
-  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjYopLx9oXcAhWMt1kKHTiSCSQQjRx6BAgBEAU&url=http%3A%2F%2Fwww.vitamin-ha.com%2Ffunny-dog-shaming-pictures-15-pics%2Ffunny-dog-shaming-pictures-01%2F&psig=AOvVaw0w483W3GZVJDiULDmG1lKj&ust=1530810138691688',
+  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjYopLx9oXcAhWMt1kKHTiSCSQQjRx6BAgBEAU&url=http%3A%2F%2Fwww.vitamin-ha.com%2Ffunny-dog-shaming-pictures-15-pics%2Ffunny-dog-shaming-pictures-01%2F&psig=AOvVaw0w483W3GZVJDiULDmG1lKj&ust=1530810138691688.jpg',
   name: 'Rex',
   description: 'This is too cute')
 Image.create(
-  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi_jJf2-IXcAhWKzlkKHZtcBJwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.puppyleaks.com%2Fbond-with-new-dog%2F&psig=AOvVaw3nqni7G6sOc5o2vn5WbpZ3&ust=1530810836560488',
+  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwi_jJf2-IXcAhWKzlkKHZtcBJwQjRx6BAgBEAU&url=https%3A%2F%2Fwww.puppyleaks.com%2Fbond-with-new-dog%2F&psig=AOvVaw3nqni7G6sOc5o2vn5WbpZ3&ust=1530810836560488.jpg',
   name: 'Hudson',
   description: 'Sometimes there is nothing better than puppy love')
 Image.create(
-  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiCrqSW-YXcAhVHrVkKHTrWCW8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.buzzfeed.com%2Fnotmonroe%2Fgolden-cocker-retriever-the-forever-puppy-gg32&psig=AOvVaw3ptVWpUIUyCrOzRg3sN3ua&ust=1530810907692976',
+  imageURL: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiCrqSW-YXcAhVHrVkKHTrWCW8QjRx6BAgBEAU&url=https%3A%2F%2Fwww.buzzfeed.com%2Fnotmonroe%2Fgolden-cocker-retriever-the-forever-puppy-gg32&psig=AOvVaw3ptVWpUIUyCrOzRg3sN3ua&ust=1530810907692976.jpg',
   name: 'Golden Cocker Retriever',
   description: "Quite possibly the cutest pup in the world")
