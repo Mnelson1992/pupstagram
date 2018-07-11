0e38872c0a61c7173b823827615314dc61542db9
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-07-11 14:15:32 -0400

    Added a header to the layout

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,13 +3,17 @@ import Home from './Home'
 import Navbar from './Navbar'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import ImagesPage from '../containers/ImagesPage';
+import '../App.css';
+
 
 
 
 const App = () =>
     <Router>
       <div className="App">
+      <header className="App-header">
         <Navbar />
+      </header>
         <Route exact path="/" component={Home} />
         <Route path="/images" component={ImagesPage} />
       </div>
