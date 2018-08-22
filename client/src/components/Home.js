33e5a2387ad8eaefbccfd2d02adc53d04beb6c11
Mnelson1992 <nelson.matthew2015@gmail.com>
2018-08-21 21:19:48 -0400

    Fixed a spacing issue in the home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -4,7 +4,7 @@ import '../styles/home.css';
 const Home = () =>
   <div className="wrapper" >
     <h1> Whether you are ......</h1>
-        <h1> ....... having a bad day </h1>
+    <h1> ....... having a bad day </h1>
     <h1> or having a great day ....... </h1>
     <h1> ..... well ..... </h1>
     <h1> Pupstagram is here to make your day even better!</h1>
